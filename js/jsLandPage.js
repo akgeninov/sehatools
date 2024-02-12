5f975b78531577ecacb1e58b39e621b1152b58fd
akgeninov <akgeninov@gmail.com>
2024-02-12 07:17:52 +0700

    feat: display product

--- js/jsLandPage.js
+++ js/jsLandPage.js
@@ -36,3 +36,45 @@ function sendMessage(){
         alert('Thank You, '+params['senderName']+'! Your message has been sent'+res.status);
     })
 }
+
+
+fetch("http://localhost:3000/products")
+.then(res => res.json())
+.then(data => renderDataToContent(data));
+
+function renderDataToContent(products) {
+    let content = document.getElementById("products");
+    for (product of products) {
+        let starsHTML = '';
+        for (let i = 0; i < 5; i++) {
+            if (i < product.rate) {
+                starsHTML += '<i class="ri-star-fill"></i>';
+            } else {
+                starsHTML += '<i class="ri-star-line"></i>';
+            }
+        }
+        content.innerHTML += 
+            `<div class="list-box">
+                <div class="img-box">
+                    <img src=${product.url_image} alt="">
+                    <span class="price">$${product.price}</span>
+                    <div class="hover">
+                        <p>Click name product to see details</p>
+                    </div>
+                </div>
+                <div class="stars">
+                    ${starsHTML}
+                </div>
+                <a href="product/chocoChipCookies.html">
+                    <div class="text">
+                        <span class="name">${product.name}</span>
+                    </div>
+                </a>
+                <div class="button-shop">
+                    <a href="#" class="cart"><i class="ri-shopping-cart-2-line"></i></a>
+                    <a href="#" class="button">Order Now</a>
+                </div>
+            </div>`;
+    }
+}
+
