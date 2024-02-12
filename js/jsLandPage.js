6df11229f2d3ffd4b57c7ae8fe1e252ef177cb1c
akgeninov <akgeninov@gmail.com>
2024-02-13 01:37:10 +0700

    change path file

--- js/jsLandPage.js
+++ js/jsLandPage.js
@@ -101,3 +101,45 @@ function renderDataToContent(products) {
     }
 }
 
+//section CONTACT
+function sendMessage() {
+    // Mengambil nilai dari elemen input
+    const name = document.querySelector("input[name='name']").value;
+    const email = document.querySelector("input[name='email']").value;
+    const subject = document.querySelector("input[name='subject']").value;
+    const message = document.querySelector("textarea[name='message']").value;
+  
+    // Membentuk objek data
+    const data = {
+      name: name,
+      email: email,
+      subject: subject,
+      message: message,
+    };
+  
+    // Validasi: Memeriksa apakah semua input telah diisi
+    if (name === "" || email === "" || phone === "" || message === "") {
+      alert("Mohon lengkapi semua form sebelum mengirim pesan.");
+      return; // Menghentikan pengiriman jika ada input yang kosong
+    }
+  
+    // Mengirim data ke API
+    fetch("http://localhost:3600/add-contact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((responseData) => {
+        // Tanggapan dari API dapat digunakan di sini
+        console.log(responseData);
+        alert("Pesan Anda berhasil dikirim!");
+        kontakForm.reset();
+      })
+      .catch((error) => {
+        console.error("Terjadi kesalahan:", error);
+        alert("Terjadi kesalahan saat mengirim pesan.");
+      });
+  }
